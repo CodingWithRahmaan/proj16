bbc14cc86b70e7d33901ff1405aaf5e813f37422
CodingWithRahmaan <abdurrehmanbasra1@gmail.com>
2025-01-01 17:32:04 +0400

    .

--- app.js
+++ app.js
@@ -21,6 +21,55 @@
 // let mixedArray = ["Anas", 18, true,];
 
 
+// chapter 14 - 16
+// Q7
+
+// let qualification = ['SSC', 'HSC', 'BSC', 'MSC', 'PhD'];
+// document.write(`<h1>Qualification</h1> <br> 1.) ${qualification[0]}<br> 2.) ${qualification[1]} </h1> <br> 3.) ${qualification[2]}<br> 4.) ${qualification[3]} `)
+
+
+// let firstName = `anas`
+// console.log(`${firstName[0].toUpperCase()}${firstName.slice(1)}`)
+
+
+// let arr =  ['Karachi' , 'Laawhore' , 'Islamabad' ]
+
+// console.log(arr.reverse())
+
+// question 8
+
+// let students = ['Anas', 'Waqar', 'Rahmaan', 'Ahmad']
+// let marks = [400, 350, 370, 420]
+// const TOTAL = 500
+
+// document.write(`Score of ${students[0]} is ${marks[0]}. Percentage ${marks[0] / TOTAL * 100}% <br> `)
+
+// document.write(`Score of ${students[1]} is ${marks[1]}. Percentage ${marks[1] / TOTAL * 100}% <br> `)
+
+// document.write(`Score of ${students[2]} is ${marks[2]}. Percentage ${marks[2] / TOTAL * 100}% <br> `)
+
+// document.write(`Score of ${students[3]} is ${marks[3]}. Percentage ${marks[3] / TOTAL * 100}% <br> `)
+
+
+
+// question 10
+
+// let marks = [400, 350, 370, 420]
+
+// document.write(`${marks} <br>`)
+
+// marks.sort((a, b) => a - b)
+
+// document.write(`${marks}`)
+
+
+
+
+// question 12
+
+// let arr = ['This', 'is', 'my' , 'cat'];
+
+// document.write(`${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} <br> `)
 
 
 
@@ -28,7 +77,27 @@
 
 
 
+// question 13
 
+// let devices = ['Keyboard', 'mouse', 'printer', 'monitor']
+
+// document.write(`${devices.shift()} <br>`)
+// document.write(`${devices.shift()} <br>`)
+// document.write(`${devices.shift()} <br>`)
+// document.write(`${devices.shift()} <br>`)
+
+
+
+
+
+// question 14
+
+// let devices = ['Keyboard', 'mouse', 'printer', 'monitor']
+
+// document.write(`${devices.pop()} <br>`)
+// document.write(`${devices.pop()} <br>`)
+// document.write(`${devices.pop()} <br>`)
+// document.write(`${devices.pop()} <br>`)
 
 
 
